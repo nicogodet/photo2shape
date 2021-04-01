4af9630edc350cb5d75d0479fcd212cbd6787c67
nicogodet <nicolas.godet@outlook.fr>
2021-04-01 18:48:06 +0200

    Update fr trad

--- i18n/photo2shape_fr.ts
+++ i18n/photo2shape_fr.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="fr_FR" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -10,12 +11,12 @@
     <message>
         <location filename="../gui/aboutdialog.py" line="57"/>
         <source>About {}</source>
-        <translation type="unfinished"></translation>
+        <translation>À propos</translation>
     </message>
     <message>
         <location filename="../gui/aboutdialog.py" line="63"/>
         <source>&lt;p&gt;{description}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: {developer}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;{homepage}&quot;&gt;{homepage}&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;{bugtracker}&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;{description}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Développeurs&lt;/strong&gt; : {developer}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Page d&apos;accueil&lt;/strong&gt; : &lt;a href=&quot;{homepage}&quot;&gt;{homepage}&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Merci de remonter les problèmes sur le &lt;a href=&quot;{bugtracker}&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</translation>
     </message>
 </context>
 <context>
@@ -48,22 +49,22 @@
     <message>
         <location filename="../ui/photo2shapedialogbase.ui" line="20"/>
         <source>Output file</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier de sortie</translation>
     </message>
     <message>
         <location filename="../ui/photo2shapedialogbase.ui" line="27"/>
         <source>Append to the existing file</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajouter au fichier existant</translation>
     </message>
     <message>
         <location filename="../ui/photo2shapedialogbase.ui" line="64"/>
         <source>Encoding</source>
-        <translation type="unfinished"></translation>
+        <translation>Encodage</translation>
     </message>
     <message>
         <location filename="../ui/photo2shapedialogbase.ui" line="81"/>
         <source>Backend to use</source>
-        <translation type="unfinished"></translation>
+        <translation>Traitement à utiliser</translation>
     </message>
     <message>
         <location filename="../ui/aboutdialogbase.ui" line="63"/>
@@ -72,7 +73,7 @@
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -80,12 +81,12 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../backends/exifpybackend.py" line="50"/>
         <source>There are no GPS tags in the file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il n&apos;y a pas de metadata GPS dans le fichier.</translation>
     </message>
     <message>
         <location filename="../backends/exifpybackend.py" line="55"/>
         <source>There are no GPS fix data.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il n&apos;y a pas de données GPS dans le fichier.</translation>
     </message>
 </context>
 <context>
@@ -93,17 +94,17 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../backends/gdalbackend.py" line="52"/>
         <source>Failed to open file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Echec lors de l&apos;ouverture du fichier.</translation>
     </message>
     <message>
         <location filename="../backends/gdalbackend.py" line="60"/>
         <source>There are no GPS tags in the file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il n&apos;y a pas de metadata GPS dans le fichier.</translation>
     </message>
     <message>
         <location filename="../backends/gdalbackend.py" line="65"/>
         <source>There are no GPS fix data.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il n&apos;y a pas de données GPS dans le fichier.</translation>
     </message>
 </context>
 <context>
@@ -131,7 +132,7 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../gui/photo2shapedialog.py" line="94"/>
         <source>Output file is a new file and can not be used in the &quot;append&quot; mode. Either specify existing file or uncheck corresponding checkbox.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le fichier spécifié est un nouveau fichier et ne peut pas être utilisé en mode &quot;ajout&quot;. Spécifiez un fichier existant ou découchez la case correspondante.</translation>
     </message>
 </context>
 <context>
@@ -143,13 +144,13 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <location filename="../photo2shapeplugin.py" line="61"/>
-        <source>About Photo2Shape&#xe2;&#x80;&#xa6;</source>
+        <source>About Photo2Shapeâ¦</source>
         <translation>À propos de Photo2Shape...</translation>
     </message>
     <message>
         <location filename="../photo2shapeplugin.py" line="109"/>
         <source>Photos imported successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>Photos importées avec succès.</translation>
     </message>
 </context>
 <context>
@@ -157,22 +158,22 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../photoimportertask.py" line="77"/>
         <source>Unable to open or create layer.</source>
-        <translation type="unfinished">Impossible d&apos;ouvrir ou de créer le fichier.</translation>
+        <translation>Impossible d&apos;ouvrir ou de créer le fichier.</translation>
     </message>
     <message>
         <location filename="../photoimportertask.py" line="93"/>
         <source>No images found in the directory.</source>
-        <translation type="unfinished">Aucune image trouvé dans le dossier.</translation>
+        <translation>Aucune image trouvé dans le dossier.</translation>
     </message>
     <message>
         <location filename="../photoimportertask.py" line="107"/>
         <source>Skipping file {fileName}: {error}</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier {fileName} ignoré : {error}</translation>
     </message>
     <message>
         <location filename="../photoimportertask.py" line="147"/>
         <source>File has incorrect WKB type &quot;{}&quot;. Please select layer with &quot;PointZ&quot; WKB type.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le fichier de type WBK &quot;{}&quot; est incorrect. Merci de choisir une couche de type WKB &quot;PointZ&quot;.</translation>
     </message>
 </context>
 </TS>
